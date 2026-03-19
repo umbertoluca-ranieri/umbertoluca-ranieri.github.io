f6ae1a0d726e478e8e8fd3abfd6d4d6c9755ace4
umbertoluca-ranieri <myself4102@gmail.com>
2026-03-19 22:05:34 +0000

    Deploying to gh-pages from @ umbertoluca-ranieri/umbertoluca-ranieri.github.io@106811d6378e2e9cd008771e31d97baceb789e40 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,46 +469,18 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%75%6D%62%65%72%74%6F%6C%75%63%61.%72%61%6E%69%65%72%69@%65%68%75.%65%75%73", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=uIu5NpIAAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
