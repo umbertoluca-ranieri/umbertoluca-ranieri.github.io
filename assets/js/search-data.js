562483181a5d9ef7c60c0a7e422569ed73c7754c
umbertoluca-ranieri <269561751+umbertoluca-ranieri@users.noreply.github.com>
2026-03-21 10:21:58 +0000

    Deploying to gh-pages from @ umbertoluca-ranieri/umbertoluca-ranieri.github.io@edac0af7685ddb018c5780632b32194e44a5580d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/gallery/";
           },
+        },{id: "nav-gallery",
+          title: "Gallery",
+          description: "Some photos I took in the lab over the years...",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/gallery/";
+          },
         },{id: "nav-contact",
           title: "Contact",
           description: "",
