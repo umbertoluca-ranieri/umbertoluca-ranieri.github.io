f7b7ed03a0d9265e26a163eb34cee342c6593b77
umbertoluca-ranieri <269561751+umbertoluca-ranieri@users.noreply.github.com>
2026-06-12 16:03:15 +0000

    Deploying to gh-pages from @ umbertoluca-ranieri/umbertoluca-ranieri.github.io@9b69f644093995516774b6dfca9724c52e21b148 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Research/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects2/";
+          },
         },{id: "nav-publications",
           title: "Publications",
           description: "",
