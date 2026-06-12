bfca9008c6c3a10560d7a4e84d8f317b839d1d7f
umbertoluca-ranieri <269561751+umbertoluca-ranieri@users.noreply.github.com>
2026-06-12 13:34:25 +0000

    Deploying to gh-pages from @ umbertoluca-ranieri/umbertoluca-ranieri.github.io@1425b255454cb15c98ba5cf2ae60362db38d68e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,7 +412,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-prof-elena-solana-madruga-universidad-complutense-de-madrid-spain-will-visit-our-group-and-will-be-giving-a-seminar-as-part-of-the-ongoing-neutron-scattering-series",
+            },},{id: "news-prof-elena-solana-madruga-universidad-complutense-de-madrid-spain-will-visit-our-group-and-will-be-giving-a-seminar-as-part-of-the-ongoing-cfm-neutron-scattering-series",
           title: 'Prof. Elena Solana Madruga (Universidad Complutense de Madrid, Spain) will visit our group...',
           description: "",
           section: "News",},{id: "news-we-will-be-hosting-a-seminar-by-prof-elisa-borfecchia-university-of-turin-italy-as-part-of-the-ongoing-cfm-colloquium-series",
